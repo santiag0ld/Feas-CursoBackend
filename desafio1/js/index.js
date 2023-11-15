f41e0c6add4b0f08f4d45b30fe7876fc4afbfa27
Santiago Feas <sant.feas@gmail.com>
2023-11-14 21:01:30 -0300

    Desafio 2

--- desafio1/js/index.js
+++ desafio1/js/index.js
@@ -1,107 +1,59 @@
-const fs = require('fs')
-
-class ProductManager {
-  constructor (path) {
-    this.path = path || "products.json"
-    this.counterId = 0
-    this.products = []
-    /*{
-        id 
-        title (nombre del producto)
-        description (descripción del producto)
-        price (precio)
-        code (código identificador)
-        thumbnail (ruta de imagen)
-        stock (número de piezas disponibles)
-      }*/
-    
-    // inicializa productos
-    this.getProducts(true)
-  }
-
-  getProducts = async (sincroniza = false) => {
-    let obtainProducts
-    const verSiExiste = fs.existsSync(this.path)
-    if (!verSiExiste) {
-      obtainProducts = []
-    } else {
-      obtainProducts = await fs.promises.readFile(this.path, "utf-8")
-      obtainProducts = JSON.parse(obtainProducts)
+class Product {
+    constructor(id, title, description, price, thumbnail, code, stock) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
     }
-
-    if (sincroniza) { this.products = obtainProducts }
-
-    return obtainProducts
-  }
-
-  getProductsById = async (id) => {
-    let leerProductos = []
-    leerProductos = await this.getProducts()
-
-    const producto = leerProductos.find((prod) => prod.id === id)
-    return producto ? producto : "Producto no encontrado"
-  }
-
-  async addProduct ( { title, description, price, code, thumbnail, stock } ) {
-
-    if (!title || !description || !price || !code || !thumbnail || !stock ) {
-      return "ERROR: debe completar todos los campos"
-    }
-
-    const existe = this.products.some((p) => p.code === code);
-    if (existe) {
-      return "ERROR: codigo repetido"
-    }
-
-    const newProduct = {
-      id: this.counterId,
-      title: title,
-      description: description,
-      price: price,
-      thumbnail: thumbnail,
-      code: code,
-      stock: stock
-    }
-
-    this.counterId++
-    this.products.push(newProduct)
-    const jsonProduct = JSON.stringify(this.products, null, 2)
-    await fs.promises.writeFile(this.path, jsonProduct)
-    return this.products
-  }
-
-  async updateProduct ( id, cambioObj ) {
-    const i = this.products.findIndex(elm => elm.id===id)
-
-    if (i === -1) {
-      return "Producto no encontrado";
-    } else {
-      const keys = Object.keys(cambioObj)
-      keys.forEach((k) => {
-        this.products[i][k]=cambioObj[k]
-      })
-    }
-    
-    const jsonProduct = JSON.stringify(this.products, null, 2)
-    await fs.promises.writeFile(this.path, jsonProduct)
-    return this.products
-  }
-
-  async deleteProduct (id) {
-    const i = this.products.findIndex(elm => elm.id===id)
-
-    if (i === -1) {
-      return "Producto no encontrado";
-    } else {
-      const newProducts = this.products.filter((elm) => elm.id != id)
-      this.products = newProducts
-      const jsonProduct = JSON.stringify(this.products, null, 2)
-      await fs.promises.writeFile(this.path, jsonProduct)
-      return this.products
-    }
-  }
 }
 
-module.exports = {
-  ProductManager: ProductManager,
-};
+class ProductManager {
+    constructor() {
+        this.products = [];
+        this.id = 1;
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.error("Por favor, complete todos los campos.");
+            return;
+        }
+        if (this.products.some(product => product.title === title)) {
+            console.error("Ya existe un producto con el mismo título.");
+            return;
+        }
+
+        const product = new Product(this.id, title, description, price, thumbnail, code, stock);
+        this.products.push(product);
+        this.id++;
+    }
+
+    getProducts() {
+        return this.products;
+    }
+
+    getProductById(code) {
+        const product = this.products.find(product => product.code === code);
+        if (!product) {
+            console.error("Not Found.");
+        }
+        return product;
+    }
+}
+
+const productManager = new ProductManager();
+
+productManager.addProduct("producto prueba", "Este es un producto de prueba", 200, "abc123", "Sin imagen", 25);
+productManager.addProduct("otra prueba", "Este es otro producto de prueba", 150, "abc124", "Sin imagen", 25);
+productManager.addProduct("tercer prueba", "Este es la tercer prueba", 150, "abc125", "Sin imagen", 25);
+productManager.addProduct("producto prueba", "Este es un producto de prueba", 200, "abc123", "Sin imagen", 25);
+productManager.addProduct("producto sin codigo");
+
+const getProducts = productManager.getProducts();
+console.log(getProducts);
+
+const productById = productManager.getProductById();
+console.log(productById);
